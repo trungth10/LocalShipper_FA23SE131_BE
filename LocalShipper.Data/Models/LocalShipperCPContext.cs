049d5348e7067d698c2ac1da86b8b06fbde0f468
trungth10 <106751712+trungth10@users.noreply.github.com>
2023-09-16 14:49:10 +0700

    BaseProject

--- LocalShipper.Data/Models/LocalShipperCPContext.cs
+++ LocalShipper.Data/Models/LocalShipperCPContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace LocalShipper.Data.Models
 {
@@ -41,8 +42,11 @@ namespace LocalShipper.Data.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server= ADMIN-PC;Database=LocalShipperCP;Uid=sa;Pwd=12345;");
+                var builder = new ConfigurationBuilder()
+                                  .SetBasePath(Directory.GetCurrentDirectory())
+                                  .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                IConfigurationRoot configuration = builder.Build();
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("DBLocalShipper"));
             }
         }
 
